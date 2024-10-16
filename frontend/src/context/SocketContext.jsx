264b92d58980d3ec22f6e0c3bf1ec82b8c976941
Ayub40 <ayubk4028@gmail.com>
2024-10-16 21:53:19 +0600

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            // const socket = io("http://localhost:5000", {
+            const socket = io("https://real-time-chat-h6f8.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
